aafc390bb9e268343eb70e2653970a70f0fe97a4
WESt <https://github.com/WEStWEStWESt>
2018-09-15 19:17:49 +0300

    created Reader class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" ! ");
+        System.out.println(args[0]);
+        try {
+            System.out.println(new Reader(args[0]).read());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/Reader.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reader {
+
+    private String s;
+
+    public Reader(String s) {
+        this.s = s;
+    }
+
+    public List<String> read() throws IOException{
+
+        ArrayList<String> list = new ArrayList<>();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(s))){
+
+            String inp ;
+
+            while ((inp = reader.readLine()) != null){
+                list.add(inp);
+            }
+
+        }
+        return list;
+    }
+
+
+}
